3f35f68db0f56669fcaeb2d59ce82837e618e261
Adam Hrycyk <ahrycyk@manitoba-ehealth.ca>
2018-11-07 10:41:56 -0600

    small cleanup

--- src/objects/Bullet.js
+++ src/objects/Bullet.js
@@ -7,7 +7,6 @@ class Bullet extends Phaser.GameObjects.Image {
         Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet');
         this.speed = 500;
         this.born = 0;
-        this.direction = 0;
         this.xSpeed = 0;
         this.ySpeed = 0;
         this.setSize(12, 12, true);
@@ -20,21 +19,17 @@ class Bullet extends Phaser.GameObjects.Image {
         // i think the player sprite is rotated from what is logical
         // ie we dont shoot bullets out of his 0 degre, we shoot bullets out of his 90 degree
         // or something
-        this.direction = Math.atan((target.x - shooter.x) / (target.y - shooter.y));
+        let direction = Math.atan((target.x - shooter.x) / (target.y - shooter.y));
         this.setPosition(shooter.x + (this.gunLength * Math.cos(shooter.rotation)), shooter.y + (this.gunLength * Math.sin(shooter.rotation)));
 
         // Calculate X and y velocity of bullet to moves it from shooter to target
         if (target.y >= shooter.y) {
-            /*this.xSpeed = this.speed * Math.sin(this.direction);
-            this.ySpeed = this.speed * Math.cos(this.direction);*/
-            this.body.setVelocityX(this.speed * Math.sin(this.direction));
-            this.body.setVelocityY(this.speed * Math.cos(this.direction));
+            this.body.setVelocityX(this.speed * Math.sin(direction));
+            this.body.setVelocityY(this.speed * Math.cos(direction));
         }
         else {
-            this.body.setVelocityX(-this.speed * Math.sin(this.direction));
-            this.body.setVelocityY(-this.speed * Math.cos(this.direction));
-            /*this.xSpeed = -this.speed * Math.sin(this.direction);
-            this.ySpeed = -this.speed * Math.cos(this.direction);*/
+            this.body.setVelocityX(-this.speed * Math.sin(direction));
+            this.body.setVelocityY(-this.speed * Math.cos(direction));
         }
 
         this.rotation = shooter.rotation; // angle bullet with shooters rotation
@@ -42,8 +37,6 @@ class Bullet extends Phaser.GameObjects.Image {
     }
 
     update (time, delta) {
-        this.x += this.xSpeed * delta;
-        this.y += this.ySpeed * delta;
         this.born += delta;
         if (this.born > 1800) {
             this.setActive(false);
